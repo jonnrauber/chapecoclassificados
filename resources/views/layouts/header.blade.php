6798347ac64bc14abffea388db566d3582ed9569
jonnrauber <natanrauber97@gmail.com>
2016-06-22 18:26:04 -0300

    Corrigido bug na visualização de paginas quando usuario não estava logado.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -131,7 +131,6 @@
             @endif
           </ul>
         </div>
-        </div>
       </nav>
 
       @yield('body')
@@ -152,6 +151,7 @@
   			</div>
     	</footer>
     </div>
+  </div>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
